43745f76a26722b19a7b1b15ee2eeb26dd4fbb9e
ganguo <ganguo>
2026-06-29 09:15:55 +0800

    action definition

---
+++ src/main/java/com/doublegsoft/jcommons/metavalue/ActionDefinition.java
@@ -0,0 +1,44 @@
+package com.doublegsoft.jcommons.metavalue;
+
+public class ActionDefinition {
+
+  private String resource;
+
+  private String path;
+
+  private String method;
+
+  private String type;
+
+  public String getResource() {
+    return resource;
+  }
+
+  public void setResource(String resource) {
+    this.resource = resource;
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public void setPath(String path) {
+    this.path = path;
+  }
+
+  public String getMethod() {
+    return method;
+  }
+
+  public void setMethod(String method) {
+    this.method = method;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+}
